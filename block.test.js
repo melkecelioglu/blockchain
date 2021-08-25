06be2e5fcdb060b7949c674ca6aa83e5a547eba6
Melike KECELIOGLU <melkeceli@icloud.com>
2021-08-25 19:07:22 +0300

    v2 changes

--- block.test.js
+++ block.test.js
@@ -5,7 +5,12 @@ describe('Block', () => {
     const lastHash = 'foo-hash';
     const hash = 'bar-hash';
     const data = ['blockchain', 'data'];
+    const nonce = 1;
+    const difficulty = 1;
+    const block = new Block({ timestamp, lastHash, hash, data, nonce, difficulty});
+
     const block= new Block({ timestamp, lastHash, hash, data });
+
 
     it('has a timestamp, lasthash, hash and data property', () => {
         expect(block.timestamp).toEqual(timestamp);

537ec087af5a341e7d2a127020d2503bf3db0e7d
Melike KECELIOGLU <melkeceli@icloud.com>
2021-09-07 00:18:24 +0300

    v2

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './components/App';
 
 render(
-<div> Cryptochain in React! </div>, 
-document.getElementById('root'));
+//<div> Cryptochain in React! : Live Update </div>, 
+  <App />,
+document.getElementById('root')
+);

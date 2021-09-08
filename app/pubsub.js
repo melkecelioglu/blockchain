65de2b3d83e0e25e0bd9c4b5129e8db3ccfb6f8c
Melike KECELIOGLU <melkeceli@icloud.com>
2021-09-08 23:05:56 +0300

    this cryptochain peoject completely done

--- app/pubsub.js
+++ app/pubsub.js
@@ -1,76 +1,75 @@
 const redis = require('redis');
 
 const CHANNELS = {
-    TEST: 'TEST',
-    BLOCKCHAIN: 'BLOCKCHAIN',
-    TRANSACTION ='TRANSACTION'
+  TEST: 'TEST',
+  BLOCKCHAIN: 'BLOCKCHAIN',
+  TRANSACTION: 'TRANSACTION'
 };
 
-class PubSub{
-    constructor({blockchain}){
+class PubSub {
+  constructor({ blockchain, transactionPool, redisUrl }) {
+    this.blockchain = blockchain;
+    this.transactionPool = transactionPool;
 
-        this.blockchain=blockchain;
-        this.tarnsactionPool = transactionPool;
+    this.publisher = redis.createClient(redisUrl);
+    this.subscriber = redis.createClient(redisUrl);
 
-        this.publisher = redis.createClient();
-        this.subscriber = redis.createClient();
+    this.subscribeToChannels();
 
-        this.subscribeToChannels();
+    this.subscriber.on(
+      'message',
+      (channel, message) => this.handleMessage(channel, message)
+    );
+  }
 
-        this.subscriber.subscribe(CHANNELS.TEST);
-        this.subscriber.subscribe(CHANNELS.BLOCKCHAIN);
+  handleMessage(channel, message) {
+    console.log(`Message received. Channel: ${channel}. Message: ${message}.`);
 
-        this.subscriber.on(
-            'message',
-            (channel, message )=> this.handeMessgae(channel,message)
-        );
-    }
+    const parsedMessage = JSON.parse(message);
 
-    handleMessage(channel, message){
-        console.log(`Message received. Chanel: ${channel}. Message: ${message}.`);
-
-        const parsedMessage = JSON.parse(message);
-
-        switch(channel){
-            case CHANNELS.BLOCKCHAIN:
-            this.blockchain.replaceChain(parsedMessage, () => {
-                this.transactionPool.clearBlockchainTransactions({
-                    chain: parsedMessage
-                });
-            });
+    switch(channel) {
+      case CHANNELS.BLOCKCHAIN:
+        this.blockchain.replaceChain(parsedMessage, true, () => {
+          this.transactionPool.clearBlockchainTransactions({
+            chain: parsedMessage
+          });
+        });
         break;
-        case CHANNELS.TRANSACTION:
-            this.transactionPool.setTransaction(parsedMessage);
-            break;
-            default:
-                return;
-        }
+      case CHANNELS.TRANSACTION:
+        this.transactionPool.setTransaction(parsedMessage);
+        break;
+      default:
+        return;
     }
+  }
 
-    subscribeToChannels(){
-        Object.values(CHANNELS).forEach(channel => {
-            this.subscriber.subscribe(channel);
-        });
-    }
-    publish({ channel, message}){
-        this.subscriber.unsubscribe(channel, () =>{
-           this.publisher.publish(channel, message, () =>{
-            this.subscriber.subscribe(channel);
-           });
-        });
-    }
-    
-    broadcastChain(){
-        this.publish({
-            channel: CHANNELS.BLOCKCHAIN,
-            message: JSON.stringify(this.blockchain.chain)
-        });
-    }
-    broadcastTransaction(transaction){
-        this.publish({
-            channel: CHANNELS.TRANSACTION,
-            message: JSON.stringify(transaction)
-        });
-    }
+  subscribeToChannels() {
+    Object.values(CHANNELS).forEach(channel => {
+      this.subscriber.subscribe(channel);
+    });
+  }
+
+  publish({ channel, message }) {
+    this.subscriber.unsubscribe(channel, () => {
+      this.publisher.publish(channel, message, () => {
+        this.subscriber.subscribe(channel);
+      });
+    });
+  }
+
+  broadcastChain() {
+    this.publish({
+      channel: CHANNELS.BLOCKCHAIN,
+      message: JSON.stringify(this.blockchain.chain)
+    });
+  }
+
+  broadcastTransaction(transaction) {
+    this.publish({
+      channel: CHANNELS.TRANSACTION,
+      message: JSON.stringify(transaction)
+    });
+  }
 }
-module.exports =PubSub;
+
+module.exports = PubSub;
